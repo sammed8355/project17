37213ca8a1be2d2f16fda28ce4305232dea5c22d
Sammed <sammed8355@gmail.com>
2026-05-03 17:43:20 +0530

    Fix package.json and push project

--- push_to_github.bat
+++ push_to_github.bat
@@ -3,8 +3,10 @@ echo ===========================================
 echo Pushing Mahila Bachat Gat to GitHub...
 echo ===========================================
 git init
+git config user.email "sammed8355@gmail.com"
+git config user.name "Sammed"
 git add .
-git commit -m "Full Mahila Bachat Gat Project Upload"
+git commit -m "Fix package.json and push project"
 git branch -M main
 git remote add origin https://github.com/sammed8355/project17.git
 git push -u origin main
